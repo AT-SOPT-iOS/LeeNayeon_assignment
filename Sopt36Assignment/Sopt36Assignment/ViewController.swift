e86b2967903cab0f611e9657952e36e69e861c9d
yeonee <leenayeon@sookmyung.ac.kr>
2025-04-15 02:39:54 +0900

    chore: 초기세팅

---
+++ Sopt36Assignment/Sopt36Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt36Assignment
+//
+//  Created by 이나연 on 4/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
