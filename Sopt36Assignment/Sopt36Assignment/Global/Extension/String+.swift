9341137a2ac93f8f3b87a45f1569108e8d257bb7
yeonee <leenayeon@sookmyung.ac.kr>
2025-04-22 02:55:19 +0900

    feat: string extension

---
+++ Sopt36Assignment/Sopt36Assignment/Global/Extension/String+.swift
@@ -0,0 +1,24 @@
+//
+//  String+.swift
+//  Sopt36Assignment
+//
+//  Created by 이나연 on 4/22/25.
+//
+
+import UIKit
+extension String{
+    func isValidEmail(email: String) -> Bool{
+        let regExp = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        return NSPredicate(format: "SELF MATCHES %@", regExp).evaluate(with: self)
+    }
+    
+    func isValidNickname(nickName: String) -> Bool{
+        let regExp = "ㄱ-|가-힣"
+        return NSPredicate(format: "SELF MATCHES %@", regExp).evaluate(with: self)
+    }
+    
+    func isValidPassword(password: String) -> Bool{
+        let regExp = #"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$"#
+        return NSPredicate(format: "SELF MATCHES %@", regExp).evaluate(with: self)
+    }
+}
