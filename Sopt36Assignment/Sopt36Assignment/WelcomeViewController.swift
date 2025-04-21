31420ed09da49ae805a9e84a2bc9756363bbacaf
yeonee <leenayeon@sookmyung.ac.kr>
2025-04-22 01:24:19 +0900

    feat: welcome view UI

--- Sopt36Assignment/Sopt36Assignment/WelcomeViewController.swift
+++ Sopt36Assignment/Sopt36Assignment/WelcomeViewController.swift
@@ -6,13 +6,76 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class WelcomeViewController: UIViewController {
     
+    //MARK: - UIComponent
+    private lazy var tvingImg = UIImageView().then{
+        $0.image = .tvingImg
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private lazy var welcomeLabel = UILabel().then{
+        $0.text = "atsopt@naver.com님\n반가워요!"
+        $0.font = .pretendard(.bold, size: 23)
+        $0.textColor = .white
+        $0.numberOfLines = 2
+        $0.setLineSpacingAndAlignment(spacing: 16.0, alignment: .center)
+    }
+    
+    private lazy var goMainButton = UIButton().then{
+        $0.setTitle("메인으로", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = UIColor(named: "Red")
+        $0.layer.cornerRadius = 3
+        $0.titleLabel?.font = .pretendard(.semiBold, size: 14)
+        $0.layer.borderWidth = 0
+    }
 }
 
 extension WelcomeViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .black
+        
+        addViews()
+        setLayout()
     }
+    
+    //MARK: - Layout
+    
+    private func addViews(){
+        self.view.addSubviews(
+            tvingImg,
+            welcomeLabel,
+            goMainButton
+        )
+    }
+    
+    private func setLayout(){
+        tvingImg.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(58)
+        }
+        
+        welcomeLabel.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(tvingImg.snp.bottom).offset(67)
+        }
+        
+        goMainButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-66)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(52)
+        }
+    }
+    
+}
+
+
+#Preview{
+    WelcomeViewController()
 }
