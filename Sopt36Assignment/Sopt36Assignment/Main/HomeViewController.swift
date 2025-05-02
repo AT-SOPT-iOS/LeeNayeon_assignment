8deb16aa835ae0f672f3eb192d5a3d90e937cfdd
yeonee <leenayeon@sookmyung.ac.kr>
2025-05-03 03:28:16 +0900

    feat: 탭바 변경에 따른 뷰 변경 구현 (#3)

--- Sopt36Assignment/Sopt36Assignment/Main/HomeViewController.swift
+++ Sopt36Assignment/Sopt36Assignment/Main/HomeViewController.swift
@@ -17,12 +17,21 @@ final class HomeViewController : UIViewController{
     private let homeTableView = UITableView(frame: .zero, style: .plain)
     
     private var headerStackview = UIStackView()
+    private var genreCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    private let genreItems = ["홈", "드라마", "예능", "영화", "스포츠", "뉴스"]
+    
+    private let indicatorView = UIView()
+    private let dividerView = UIView()
+    
     private var tvingLogoImage = UIImageView()
     private var searchIconImage = UIImageView()
     private var profileIconImage = UIImageView()
     
-    private var currentEmbeddedVC: UIViewController?
 
+    //MARK: - Property
+    private var selectedIndexPath : Int = 0
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
@@ -33,12 +42,21 @@ final class HomeViewController : UIViewController{
         setLayout()
         setDelegate()
         
-        registerHomeTableViewCell()
+        registerTableViewCell()
+
+        setGenreCollectionViewFlowLayout()
+        registerGenreCell()
         
+        if indicatorView.frame == .zero {
+            DispatchQueue.main.async {
+                let initialIndexPath = IndexPath(item: 0, section: 0)
+                self.moveIndicator(to: initialIndexPath)
+            }
+        }
     }
     
     private func setUI(){
-        self.view.addSubviews(headerStackview, homeTableView)
+        self.view.addSubviews(headerStackview, genreCollectionView, indicatorView, dividerView, homeTableView)
         
         [tvingLogoImage, searchIconImage, profileIconImage].forEach{
             headerStackview.addArrangedSubview($0)
@@ -65,14 +83,20 @@ final class HomeViewController : UIViewController{
         profileIconImage.image = UIImage(named: "profileImg")
         profileIconImage.contentMode = .scaleAspectFit
         
-        homeTableView.backgroundColor = .clear
+        genreCollectionView.backgroundColor = .clear
+        
+        indicatorView.backgroundColor = .white
+        
+        dividerView.backgroundColor = .gray2
+        
+        homeTableView.backgroundColor = .black
         homeTableView.separatorStyle = .none
         homeTableView.showsVerticalScrollIndicator = false
     }
     
     private func setLayout(){
         headerStackview.snp.makeConstraints{
-            $0.top.equalTo(view).offset(50)
+            $0.top.equalTo(view).offset(20)
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
             $0.height.equalTo(78)
@@ -88,21 +112,88 @@ final class HomeViewController : UIViewController{
             }
         }
         
+        genreCollectionView.snp.makeConstraints {
+            $0.top.equalTo(headerStackview.snp.bottom).offset(30)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(27)
+        }
+        
+        indicatorView.snp.makeConstraints {
+            $0.top.equalTo(genreCollectionView.snp.bottom).offset(10)
+            $0.height.equalTo(3)
+            $0.width.equalTo(0)
+            $0.centerX.equalToSuperview()
+        }
+        
+        moveIndicator(to: IndexPath(item: 0, section: 0))
+        
+        dividerView.snp.makeConstraints{
+            $0.top.equalTo(indicatorView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+        
+        
         homeTableView.snp.makeConstraints {
-            $0.top.equalTo(headerStackview.snp.bottom)
+            $0.top.equalTo(dividerView.snp.bottom)
             $0.leading.trailing.bottom.equalToSuperview()
         }
-
+        
     }
     
     private func setDelegate(){
+        genreCollectionView.delegate = self
+        genreCollectionView.dataSource = self
+        
         homeTableView.dataSource = self
         homeTableView.delegate = self
     }
     
-
-    private func registerHomeTableViewCell(){
+    
+    // MARK: - Genre Cell
+    private func setGenreCollectionViewFlowLayout(){
+        let flowLayout = UICollectionViewFlowLayout()
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let cellHeight : CGFloat = 27
+        let cellWidth = (screenWidth - 18 - 18 - 28) / 6
+        
+        flowLayout.itemSize = CGSize(width : cellWidth, height: cellHeight)
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.collectionView?.isScrollEnabled = false
+        flowLayout.minimumInteritemSpacing = 28
+        self.genreCollectionView.setCollectionViewLayout(flowLayout, animated: false)
+        
+    }
+    
+    private func registerGenreCell(){
+        genreCollectionView.register(GenreCollectionViewCell.self, forCellWithReuseIdentifier: GenreCollectionViewCell.identifier)
+    }
+    
+    func moveIndicator(to indexPath: IndexPath){
+        guard let cell = genreCollectionView.cellForItem(at: indexPath) else { return }
+        let labelWidth = (cell as? GenreCollectionViewCell)?.contentView.frame.width ?? 15
+        
+        indicatorView.snp.remakeConstraints {
+            $0.top.equalTo(genreCollectionView.snp.bottom).offset(10)
+            $0.height.equalTo(3)
+            $0.width.equalTo(labelWidth)
+            $0.centerX.equalTo(cell)
+        }
+        
+        UIView.animate(withDuration: 0.25) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func registerTableViewCell(){
         homeTableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.identifier)
+        homeTableView.register(DramaTableViewCell.self, forCellReuseIdentifier: DramaTableViewCell.identifier)
+        homeTableView.register(EntertainmentTableViewCell.self, forCellReuseIdentifier: EntertainmentTableViewCell.identifier)
+        homeTableView.register(MovieTableViewCell.self, forCellReuseIdentifier: MovieTableViewCell.identifier)
+        homeTableView.register(SportsTableViewCell.self, forCellReuseIdentifier: SportsTableViewCell.identifier)
+        homeTableView.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
     }
 }
 
@@ -117,9 +208,72 @@ extension HomeViewController : UITableViewDataSource {
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(
-                                withIdentifier: HomeTableViewCell.identifier,
-                                for: indexPath) as? HomeTableViewCell else { return UITableViewCell() }
+        switch(selectedIndexPath) {
+        case 0 :
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: HomeTableViewCell.identifier,
+                for: indexPath) as? HomeTableViewCell else { return UITableViewCell() }
+            return cell
+            
+        case 1 :
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: DramaTableViewCell.identifier,
+                for: indexPath) as? DramaTableViewCell else { return UITableViewCell() }
+            return cell
+            
+        case 2:
+            guard let cell = tableView.dequeueReusableCell(
+                                    withIdentifier: EntertainmentTableViewCell.identifier,
+                                    for: indexPath) as? EntertainmentTableViewCell else { return UITableViewCell() }
+            return cell
+            
+        case 3 :
+            guard let cell = tableView.dequeueReusableCell(
+                                    withIdentifier: MovieTableViewCell.identifier,
+                                    for: indexPath) as? MovieTableViewCell else { return UITableViewCell() }
+            return cell
+            
+        case 4 :
+            guard let cell = tableView.dequeueReusableCell(
+                                    withIdentifier: SportsTableViewCell.identifier,
+                                    for: indexPath) as? SportsTableViewCell else { return UITableViewCell() }
+            return cell
+            
+        case 5 :
+            guard let cell = tableView.dequeueReusableCell(
+                                    withIdentifier: NewsTableViewCell.identifier,
+                                    for: indexPath) as? NewsTableViewCell else { return UITableViewCell() }
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+}
+
+extension HomeViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return genreItems.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GenreCollectionViewCell.identifier, for: indexPath) as? GenreCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.dataBind(genreItems[indexPath.item])
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedIndexPath = indexPath.item
+        moveIndicator(to: indexPath)
+        homeTableView.reloadData()
+    }
+    
+}
+
+
+extension HomeViewController : UICollectionViewDelegate{
+    
 }
