503213349a48bc74efe27c3163e971cbb6407b8f
yeonee <leenayeon@sookmyung.ac.kr>
2025-05-27 22:31:59 +0900

    setting: swiftui 프로젝트 세팅

---
+++ TVING_SwiftUI/TVING_SwiftUI/TVING_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVING_SwiftUIApp.swift
+//  TVING_SwiftUI
+//
+//  Created by 이나연 on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TVING_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
