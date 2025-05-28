e6324ca3f1e1aa325881d6ef7b5ed4b57a7b1da5
yeonee <leenayeon@sookmyung.ac.kr>
2025-05-29 03:29:08 +0900

    [design]/#7 - 폰트 익스텐션

---
+++ TVING_SwiftUI/TVING_SwiftUI/Extension/Font+.swift
@@ -0,0 +1,17 @@
+//
+//  Font+.swift
+//  TVING_SwiftUI
+//
+//  Created by 이나연 on 5/29/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let semiBold50: Font = .custom("Pretendard-SemiBold", size: 50)
+    static let regular17: Font = .custom("Pretendard-Regular", size: 17)
+    static let regular10: Font = .custom("Pretendard-Regular", size: 10)
+    static let bold15: Font = .custom("Pretendard-Bold", size: 15)
+    static let medium12: Font = .custom("Pretendard-Medium", size: 12)
+    static let medium11: Font = .custom("Pretendard-Medium", size: 11)
+}
