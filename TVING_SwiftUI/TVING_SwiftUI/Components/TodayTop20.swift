f85a6910301efdee736c8afcd343530f67d88e6a
yeonee <leenayeon@sookmyung.ac.kr>
2025-05-29 03:31:50 +0900

    [design]/#7 - 오늘의티빙20 그리드

---
+++ TVING_SwiftUI/TVING_SwiftUI/Components/TodayTop20.swift
@@ -0,0 +1,48 @@
+//
+//  TodayTop20.swift
+//  TVING_SwiftUI
+//
+//  Created by 이나연 on 5/29/25.
+//
+
+import SwiftUI
+
+struct TodayTop20: View {
+    let dummyPoster = [
+        "movie1","movie2", "movie3", "movie4",
+        "movie1","movie2", "movie3", "movie4",
+        "movie1","movie2", "movie3", "movie4",
+        "movie1","movie2", "movie3", "movie4",
+        "movie1","movie2", "movie3", "movie4"
+    ]
+    
+    let columns = [GridItem(.fixed(146))]
+    
+    var body: some View {
+        VStack{
+            HStack {
+                Text("오늘의 티빙 TOP 20")
+                    .font(.bold15)
+                    .foregroundColor(Color.white)
+                    .padding(.leading, 12)
+                Spacer()
+            }
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHGrid(rows: columns, spacing: 12) {
+                    ForEach(0...dummyPoster.count-1, id: \.self) { index in
+                        HStack {
+                            Text("\(index + 1)")
+                                .font(.semiBold50)
+                                .foregroundColor(Color.white)
+                                .frame(maxHeight: .infinity, alignment: .bottom)
+                            
+                            Image("\(dummyPoster[index])")
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
